6b6c797dcfa964909c89c2f0e60fb9b29aeaeffe
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-08-24 15:53:51 -0700

    Add display function using DOM manipulation

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function Book(title, author, numPages, isRead) {
 Book.prototype.info = function() {
   let infoString = `${this.title} by ${this.author}, ${this.numPages} pages, `; 
   if(this.isRead) {
-    infoString += `has been read.`; 
+    infoString += `read`; 
   } else {
-    infoString += `not read yet.`
+    infoString += `not read`
   }
   return infoString; 
 }
@@ -20,3 +20,20 @@ function addBookToLibrary(title, author, numPages, isRead) {
   const newBook = new Book(title, author, numPages, isRead); 
   myLibrary.push(newBook); 
 }
+
+addBookToLibrary("The College", "Henry Coolidge", 201, false); 
+addBookToLibrary("The Calculator", "Jeffrey Mister", 312, true); 
+addBookToLibrary("The Book", "Arthur Wright", 452, true); 
+
+const bookCardsSection = document.querySelector("div.bookcards-wrapper"); 
+
+function displayLibrary() {
+  for(let i = 0; i < myLibrary.length; ++i) {
+    const bookCard = document.createElement("div"); 
+    bookCard.classList.add("bookcard"); 
+    bookCard.textContent = myLibrary[i].info(); 
+    bookCardsSection.insertBefore(bookCard, bookCardsSection.lastElementChild);
+  }
+}
+
+displayLibrary();
