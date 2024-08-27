635337b4aa7b59cbe93404b16050babf11a5702f
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-08-26 17:56:01 -0700

    Finish adding bookcard elements dynamically

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Book(title, author, numPages, isRead) {
   this.isRead = isRead; 
 }
 Book.prototype.info = function() {
-  let infoString = `${this.title} by ${this.author}, ${this.numPages} pages, `; 
+  let infoString = `"${this.title}" \nby \n${this.author} \n${this.numPages} pages\n`; 
   if(this.isRead) {
     infoString += `read`; 
   } else {
@@ -31,7 +31,31 @@ function displayLibrary() {
   for(let i = 0; i < myLibrary.length; ++i) {
     const bookCard = document.createElement("div"); 
     bookCard.classList.add("bookcard"); 
-    bookCard.textContent = myLibrary[i].info(); 
+    const bookText = document.createElement("p"); 
+    bookText.textContent = myLibrary[i].info();
+    bookCard.appendChild(bookText);
+    const deleteBtn = document.createElement("button"); 
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.textContent = "Remove"; 
+    bookCard.appendChild(deleteBtn);
+    const readBtn = document.createElement("button"); 
+    readBtn.classList.add("read-btn"); 
+    if(myLibrary[i].isRead) {
+      readBtn.textContent = "Not read"; 
+      readBtn.classList.add("not-read"); 
+    } else {
+      readBtn.textContent = "Read"; 
+      readBtn.classList.add("read"); 
+    }
+    bookCard.appendChild(readBtn);
+    const favBtn = document.createElement("a"); 
+    const starIcon = document.createElement("img"); 
+    favBtn.classList.add("fav-btn");
+    starIcon.src = "./images/star-outline.svg"; 
+    starIcon.height = 25;
+    starIcon.alt = "A star used to favorite books"; 
+    favBtn.appendChild(starIcon); 
+    bookCard.appendChild(favBtn);
     bookCardsSection.insertBefore(bookCard, bookCardsSection.lastElementChild);
   }
 }
